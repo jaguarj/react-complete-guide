365e18f0a36d70e61a3b79b77d0ae8ad5461a9ef
Josh Seipel <joshseipel@gmail.com>
2021-12-17 10:27:43 -0500

    Created CSS modules for Button and CourseInput.

--- 01-starting-project-course-goals/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 01-starting-project-course-goals/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
-
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import styles from './CourseInput.module.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -25,7 +24,7 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? 'invalid' : '' }`}>
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
