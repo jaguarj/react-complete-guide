5357b13e7b0839fa7da35780a706dcd142de3782
Josh Seipel <joshseipel@gmail.com>
2022-01-11 12:21:52 -0500

    Updated user form to reset values after submit.

--- 01-starter-project-add-user/src/components/Users/AddUser.js
+++ 01-starter-project-add-user/src/components/Users/AddUser.js
@@ -10,6 +10,8 @@ const AddUser = () => {
     const addUserHandler = (e) => {
         e.preventDefault();
         console.log("enteredUsername", enteredUsername, "enteredAge", enteredAge)
+        setEnteredUsername("");
+        setEnteredAge("");
     };
 
     const usernameChangeHandler = (e) => {
@@ -24,9 +26,9 @@ const AddUser = () => {
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
-                <input id="username" type="text" onChange={usernameChangeHandler}/>
+                <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler}/>
                 <label htmlFor="age">Age (Years)</label>
-                <input id="age" type="number" onChange={ageChangeHandler}/>
+                <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler}/>
                 <Button type="submit" onClick={addUserHandler}>Add User</Button>
             </form>
         </Card>
