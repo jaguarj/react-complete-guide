66b1d2f6de99b7ff0d31317c4ef2cfe3f234532f
Josh Seipel <joshseipel@gmail.com>
2022-01-13 11:58:28 -0500

    Added portals to app.

--- 01-starter-project-add-user/src/App.js
+++ 01-starter-project-add-user/src/App.js
@@ -4,16 +4,16 @@ import AddUser from './components/Users/AddUser';
 import UsersList from './components/Users/UsersList';
 
 const testUsersList = [
-  {
-    id: Math.random().toString(),
-    age: 32,
-    name: "Mac"
-  },
-  {
-    id: Math.random().toString(),
-    age: 20,
-    name: "Kim"
-  }
+	{
+		id: Math.random().toString(),
+		age: 32,
+		name: "Mac"
+	},
+	{
+		id: Math.random().toString(),
+		age: 20,
+		name: "Kim"
+	}
 ];
 
 function App() {
@@ -21,35 +21,35 @@ function App() {
  const [usersList, setUsersList] = useState(testUsersList);
 
  const addUserHandler = (userName, userAge) => {
-  setUsersList((prevUsersList) => {
-    return [...prevUsersList, {id: Math.random().toString(), name: userName, age: userAge}];
-  });
+	setUsersList((prevUsersList) => {
+		return [...prevUsersList, {id: Math.random().toString(), name: userName, age: userAge}];
+	});
  };
 
-  return (
-    <Fragment>
-      <AddUser onAddUser={addUserHandler}/>
-      <UsersList userDataList={usersList}/>
-    </Fragment>
-    
+	return (
+		<Fragment>
+			<AddUser onAddUser={addUserHandler}/>
+			<UsersList userDataList={usersList}/>
+		</Fragment>
+		
 
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
+		// <div className="App">
+		//   <header className="App-header">
+		//     <img src={logo} className="App-logo" alt="logo" />
+		//     <p>
+		//       Edit <code>src/App.js</code> and save to reload.
+		//     </p>
+		//     <a
+		//       className="App-link"
+		//       href="https://reactjs.org"
+		//       target="_blank"
+		//       rel="noopener noreferrer"
+		//     >
+		//       Learn React
+		//     </a>
+		//   </header>
+		// </div>
+	);
 }
 
 export default App;
--- 01-starter-project-add-user/src/components/UI/ErrorModal/ErrorModal.js
+++ 01-starter-project-add-user/src/components/UI/ErrorModal/ErrorModal.js
@@ -1,24 +1,33 @@
 import React from 'react';
+import ReactDom from 'react-dom';
 import Card from '../Card/Card';
 import Button from '../Button/Button';
 import classes from './ErrorModal.module.css';
 
+const Backdrop = (props) => {
+	return <div className={classes.backdrop} onClick={props.onConfirm}/>
+};
+
+const ModalOverlay = (props) => {
+	return <Card className={classes.modal}>
+		<header className={classes.header}>
+			<h2>{props.title}</h2>
+		</header>
+		<div className={classes.content}>
+			<p>{props.message}</p>
+		</div>
+		<footer className={classes.actions}>
+			<Button onClick={props.onConfirm}>Ok</Button>
+		</footer>
+	</Card>
+};
+
 const ErrorModal = (props) => {
-    return (
-        <div>
-            <div className={classes.backdrop} onClick={props.onConfirm}/>
-            <Card className={classes.modal}>
-                <header className={classes.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={classes.content}>
-                    <p>{props.message}</p>
-                </div>
-                <footer className={classes.actions}>
-                    <Button onClick={props.onConfirm}>Ok</Button>
-                </footer>
-            </Card>
-        </div>
-    )};
+	return (
+		<React.Fragment>
+			{ReactDom.createPortal(<Backdrop onConfirm={props.onConfirm}/>, document.getElementById('backdrop-root'))}
+			{ReactDom.createPortal(<ModalOverlay title={props.title} message={props.message} onConfirm={props.onConfirm}/>, document.getElementById('overlay-root'))}
+		</React.Fragment>
+	)};
 
 export default ErrorModal;
