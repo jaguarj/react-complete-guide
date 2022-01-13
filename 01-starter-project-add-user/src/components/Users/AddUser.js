30be9edb8fce0fb47a62546075a0e8fbcbd5ee79
Josh Seipel <joshseipel@gmail.com>
2022-01-13 12:10:04 -0500

    Updated indentation.

--- 01-starter-project-add-user/src/components/Users/AddUser.js
+++ 01-starter-project-add-user/src/components/Users/AddUser.js
@@ -5,62 +5,62 @@ import Button from '../UI/Button/Button'
 import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
-    const [enteredUsername, setEnteredUsername] = useState("");
-    const [enteredAge, setEnteredAge] = useState("");
-    const [error, setError] = useState();
+	const [enteredUsername, setEnteredUsername] = useState("");
+	const [enteredAge, setEnteredAge] = useState("");
+	const [error, setError] = useState();
 
-    const addUserHandler = (e) => {
-        e.preventDefault();
-        console.log("enteredUsername", enteredUsername, "enteredAge", enteredAge)
+	const addUserHandler = (e) => {
+		e.preventDefault();
+		console.log("enteredUsername", enteredUsername, "enteredAge", enteredAge)
 
-        if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
-            setError({
-                title: "Invalid input", 
-                message: "Please enter a valid input value."
-            });
-            return;
-        };
+		if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+			setError({
+				title: "Invalid input", 
+				message: "Please enter a valid input value."
+			});
+			return;
+		};
 
-        if (+enteredAge < 1) {
-            setError({
-                title: "Invalid age", 
-                message: "Please enter a valid age greater than 0."
-            });
-            return;
-        };
+		if (+enteredAge < 1) {
+			setError({
+				title: "Invalid age", 
+				message: "Please enter a valid age greater than 0."
+			});
+			return;
+		};
 
-        props.onAddUser(enteredUsername, enteredAge);
+		props.onAddUser(enteredUsername, enteredAge);
 
-        setEnteredUsername("");
-        setEnteredAge("");
-    };
+		setEnteredUsername("");
+		setEnteredAge("");
+	};
 
-    const usernameChangeHandler = (e) => {
-        setEnteredUsername(e.target.value);
-    };
+	const usernameChangeHandler = (e) => {
+		setEnteredUsername(e.target.value);
+	};
 
-    const ageChangeHandler = (e) => {
-        setEnteredAge(e.target.value);
-    };
+	const ageChangeHandler = (e) => {
+		setEnteredAge(e.target.value);
+	};
 
-    const errorHandler = () => {
-        setError(null);
-    }
+	const errorHandler = () => {
+		setError(null);
+	}
 
-    return (
-        <div>
-            {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
-            <Card className={classes.input}>
-                <form onSubmit={addUserHandler}>
-                    <label htmlFor="username">Username</label>
-                    <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler} />
-                    <label htmlFor="age">Age (Years)</label>
-                    <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler} />
-                    <Button type="submit" onClick={addUserHandler}>Add User</Button>
-                </form>
-            </Card>
-        </div>
-    )
+	return (
+		<div>
+			{error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
+			<Card className={classes.input}>
+				<form onSubmit={addUserHandler}>
+					<label htmlFor="username">Username</label>
+					<input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler} />
+					<label htmlFor="age">Age (Years)</label>
+					<input id="age" type="number" value={enteredAge} onChange={ageChangeHandler} />
+					<Button type="submit" onClick={addUserHandler}>Add User</Button>
+				</form>
+			</Card>
+		</div>
+	)
 };
 
 export default AddUser;
--- 01-starter-project-add-user/src/components/Users/UsersList.js
+++ 01-starter-project-add-user/src/components/Users/UsersList.js
@@ -4,15 +4,15 @@ import classes from './UsersList.module.css';
 
 
 const UsersList = (props) => {
-    return (
-        <Card className={classes.users}>
-            <ul>
-                {props.userDataList.map(user =>
-                    <li id={user.id} key={user.id}>{user.name} ({user.age} years old)</li>
-                )}
-            </ul>
-        </Card>
-    )
+	return (
+		<Card className={classes.users}>
+			<ul>
+				{props.userDataList.map(user =>
+					<li id={user.id} key={user.id}>{user.name} ({user.age} years old)</li>
+				)}
+			</ul>
+		</Card>
+	)
 };
 
 export default UsersList;
